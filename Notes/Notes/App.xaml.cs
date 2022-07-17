cfeed4a49fe6fab5050ac74eacd20b5712d75bae
mu <muspam922@gmail.com>
2022-07-17 20:41:55 +0100

    Replaced main window shell library

--- Notes/Notes/App.xaml.cs
+++ Notes/Notes/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Hosting;
+using Shell.Core.Extensions;
+using Shell.WPF.Shell.Controls;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -8,10 +11,42 @@ using System.Windows;
 
 namespace Notes
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private IHost _host;
+
+        public App()
+        {
+            _host = Host.CreateDefaultBuilder()
+                .ConfigureServices(s =>
+                {
+                    s.AutoRegisterDependencies(this.GetType().Assembly.GetTypes());
+                })
+                .Build();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _host.Start();
+
+            NavShell.SetServiceProvider(_host.Services);
+
+            MainWindow = new NavShell()
+            {
+
+            };
+
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await _host.StopAsync();
+            _host.Dispose();
+            _host = null;
+            base.OnExit(e);
+        }
     }
 }
